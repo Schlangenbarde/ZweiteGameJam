f332101a50121665a7f5bb22e5e9595ee9533db1
Er1cDelux3 <Er1cDelux3@gmx.de>
2022-01-06 18:37:38 +0100

    Pre-Presi

--- Skripts/Player/Player.gd
+++ Skripts/Player/Player.gd
@@ -10,7 +10,7 @@ onready var WeaponNode = $WeaponNode
 
 #PlayerStats
 var speed = 10
-var jumpHeight = 400
+var jumpHeight = 460
 var currentSpeed = Vector2()
 var PlayerLifes = 3
 
@@ -22,6 +22,10 @@ export(int) var yValue
 #Game Physics
 var gravity = 400
 var UpVector = Vector2(0, -1)
+
+
+#test
+export var on_floor = true
 
 func _ready():
 	LifeUI.text = "Lifes: " + str(PlayerLifes)
@@ -35,7 +39,10 @@ func _physics_process(delta):
 	PlayerShoot()
 	
 	move_and_slide(currentSpeed, UpVector)
+	on_floor = is_on_floor()
 	PlayerJump()
+	if is_on_ceiling():
+		currentSpeed.y = 0
 	#move_and_slide(currentSpeed, UpVector)
 
 
@@ -74,15 +81,15 @@ func PlayerSpeedControll():
 	
 	if currentSpeed.x <= -150:
 		currentSpeed.x = -150
-
+#AllowShot and LoadShot is switched due to unknown bug
 func PlayerShoot():
 	if Input.is_action_just_pressed("Shoot") and fireCooldown and !Input.is_action_pressed("Right") and !Input.is_action_pressed("Left") and is_on_floor():
-		AnimPlayer.play("LoadShot")
+		AnimPlayer.play("AllowShot")
 	
 	if Input.is_action_just_released("Shoot") and fireCooldown and !Input.is_action_pressed("Right") and !Input.is_action_pressed("Left") and is_on_floor():
 		AnimPlayer.stop(true)
 		fireCooldown = false
-		AnimPlayer.play("AllowShot")
+		AnimPlayer.play("LoadShot")
 		Weapon.shoot(xSpeed, rand_range(yValue, yValue * -1), 90)
 	
 	if Input.is_action_just_pressed("PlayerMelee") and fireCooldown and !Input.is_action_pressed("Right") and !Input.is_action_pressed("Left") and is_on_floor(): 
@@ -104,9 +111,14 @@ func GetDamage(i):
 	LifeUI.text = "Lifes: " + str(PlayerLifes)
 
 func CheckLifes():
-	if PlayerLifes == 0:
-		queue_free()
+	if PlayerLifes <= 0:
+		#queue_free()
+		get_tree().reload_current_scene()
 
 func _on_Area2D_body_entered(body):
-	AnimPlayer.play("GotDamaged")
-	GetDamage(1)
+	if !AnimPlayer.is_playing():
+		AnimPlayer.play("GotDamaged")
+		GetDamage(1)
+
+#func wait_till_reload():
+
